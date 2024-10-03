9449d938bbc69cdb57c8b325baeae19a94ff0997
Michael Stephenson <tpm2fe@bosch.com>
2024-10-03 13:54:07 +0200

    Added new controller with placeholder in route (without versioning)

---
+++ Controllers/TenantController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestVersionedApiWithOpenAPI.Controllers
+{
+  [Route("api/{tenant}/[controller]")]
+  [ApiController]
+  public class TenantController : ControllerBase
+  {
+    [HttpGet]
+    public ActionResult Get()
+    {
+      return new OkResult();
+    }
+  }
+}
